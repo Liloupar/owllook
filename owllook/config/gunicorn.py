6bc5c6725d632e51d0684c706b0a8a2d6d9f1b30
Zohar <102589617@qq.com>
2019-08-21 19:20:53 +0800

    Update gunicorn.py

--- owllook/config/gunicorn.py
+++ owllook/config/gunicorn.py
@@ -2,7 +2,7 @@
 # gunicorn -c config/gunicorn.py --worker-class sanic.worker.GunicornWorker server:app
 import os
 
-os.environ['MODE'] = 'PRO'
+os.environ['MODE'] = 'DEV'
 
 WORKERS = os.getenv('WORKERS', 2)
 TIMEOUT = os.getenv('TIMEOUT', 60)
